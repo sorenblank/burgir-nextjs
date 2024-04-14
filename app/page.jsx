900d8b124ac1515805bfa214a7bb07923d348748
sorenblank <afrozaman12@gmail.com>
2024-04-15 05:59:21 +0600

    Added CTA component to Home page

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ import Hero from "@/app/pages/Hero";
 import Promo from "@/app/pages/Promo";
 import About from "@/app/pages/About";
 import Menu from "@/app/pages/Menu";
+import CTA from "@/app/pages/CTA";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <Promo />
       <About />
       <Menu />
+      <CTA />
     </div>
     </>
   );
