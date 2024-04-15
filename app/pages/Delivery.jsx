bc09200b49ffa3117f9359952ec81a6d331d0323
sorenblank <afrozaman12@gmail.com>
2024-04-15 11:49:50 +0600

    Refactor Delivery component and update scroll behavior

--- app/pages/Delivery.jsx
+++ app/pages/Delivery.jsx
@@ -1,11 +1,40 @@
+import { useEffect, useState, useRef } from 'react';
 import { delivery } from "../lib/constants"
 import Button from "../components/Button"
 import Img from "../components/Image"
 
 const Delivery = () => {
+  const [deliveryBoyMove, setDeliveryBoyMove] = useState(-80);
+  const [lastScrollPos, setLastScrollPos] = useState(0);
+  const deliveryBoy = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let deliveryBoyTopPos = deliveryBoy.current.getBoundingClientRect().top;
+
+      if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
+        let activeScrollPos = window.scrollY;
+
+        if (lastScrollPos < activeScrollPos) {
+          setDeliveryBoyMove(deliveryBoyMove + 1);
+        } else {
+          setDeliveryBoyMove(deliveryBoyMove - 1);
+        }
+
+        setLastScrollPos(activeScrollPos);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [deliveryBoyMove, lastScrollPos]);
+
   return (
-    <section className="relative overflow-hidden md:text-left py-[60px] lg:pt-[120px] lg:pb-[60px]">
-        <div className="containerr md:grid md:grid-cols-2 md:items-center md:gap-[30px]">
+    <section id='delivery' className="relative overflow-hidden md:text-left py-[60px] lg:pt-[120px] lg:pb-[60px]">
+      <div className="containerr md:grid md:grid-cols-2 md:items-center md:gap-[30px]">
         <div>
           <h2 className="h2 font-rubik text-[32px] text-n-richblackfogra leading-[1.5] tracking-[-2px] max-w-[17ch] mx-auto md:mx-0 lg:text-[42px]">
             {delivery.title.text}{" "} <span className="text-n-yellow">{delivery.title.highlight}</span>
@@ -13,15 +42,32 @@ const Delivery = () => {
           <p className="text-[16px] mt-[25px] mb-[20px]">{delivery.description}</p>
           <Button>{delivery.buttonText}</Button>
         </div>
-
+        <figure className="relative aspect-[1/0.9]" ref={deliveryBoy}>
+          <Img
+            src={delivery.bgImage}
+            className="w-full h-auto"
+            width={700}
+            height={602}
+            alt="Delivery Background"
+            />
+          
+          <Img 
+            src={delivery.bannerImage}
+            className="absolute w-full h-auto top-0 left-0"
+            style={{ transform: `translateX(${deliveryBoyMove}px)`, transition: '0.25s ease'}}
+            width={1000}
+            height={880}
+            alt="Delivery Banner"
+            />
+        </figure>
       </div>
 
       <div
         className="absolute bottom-0 left-0 w-full h-[15px] bg-repeat bg-bottom"
-        style={{ backgroundImage: "url('/shape-white.png')" }}
+        style={{ backgroundImage: "url('/shape-grey.png')" }}
       />
     </section>
   )
 }
 
-export default Delivery
+export default Delivery;
