1ab0f1c8e5710a3cff5460aefab2656ad7a0e3dd
sorenblank <afrozaman12@gmail.com>
2024-04-15 06:27:04 +0600

    Refactor CTA component and update banner image and button alignment

--- app/pages/CTA.jsx
+++ app/pages/CTA.jsx
@@ -4,7 +4,7 @@ import { cta } from "../lib/constants";
 
 const CTA = () => {
   return (
-    <section className="relative bg-n-issabeline bg-no-repeat bg-cover bg-center text-center overflow-hidden md:text-left py-[60px] lg:py-[120px]" style={{ backgroundImage: "url('/hero-bg.jpg')" }}>
+    <section className="relative bg-n-issabeline bg-no-repeat bg-cover bg-center text-center overflow-hidden md:text-left py-[60px] lg:pt-[120px] lg:pb-[60px]" style={{ backgroundImage: "url('/hero-bg.jpg')" }}>
       <div
         className="absolute top-0 bottom-auto scale-y-[-1] left-0 w-full h-[15px] bg-repeat bg-bottom"
         style={{ backgroundImage: "url('/shape-grey.png')" }}
@@ -18,7 +18,29 @@ const CTA = () => {
           <p className="text-n-notgray text-[16px] mt-[25px] mb-[20px]">{cta.description}</p>
           <Button className="align-middle mx-auto md:mx-0">{cta.buttonText}</Button>
         </div>
+
+        <figure className="relative aspect-[1/0.9]">
+          <Img  src= {cta.bannerImage} 
+                className="w-full h-auto transform scale-[1.3] translate-y-[60px] md:translate-x-[90px] md:translate-y-[60px] lg:scale-[1.4] lg:translate-x-[20px] lg:trnaslate-y-[40px]"
+                width={700}
+                height={637}
+                alt="CTA Banner"
+            />
+
+          <Img src= {cta.saleImage}
+                className="absolute transform scale-[1] top-[50px] left-[10px] lg:left-[-50px]"
+                width={216}
+                height={216}
+                alt="Sale Shape"
+                style={{scale: '1', animation: 'scaleUp 1s linear infinite alternate'}}
+            />
+        </figure>
       </div>
+
+      <div
+        className="absolute bottom-0 left-0 w-full h-[15px] bg-repeat bg-bottom"
+        style={{ backgroundImage: "url('/shape-white.png')" }}
+      />
     </section>
   )
 }
