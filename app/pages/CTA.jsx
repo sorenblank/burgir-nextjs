bdbd5f28001c0cc58eba7460cb8382827f6ca7d1
sorenblank <afrozaman12@gmail.com>
2024-04-15 06:04:51 +0600

    Refactor CTA component and update text constants

---
+++ app/pages/CTA.jsx
@@ -0,0 +1,26 @@
+import Img from "../components/Image"
+import Button from "../components/Button";
+import { cta } from "../lib/constants";
+
+const CTA = () => {
+  return (
+    <section className="relative bg-n-issabeline bg-no-repeat bg-cover bg-center text-center overflow-hidden md:text-left py-[60px] lg:py-[120px]" style={{ backgroundImage: "url('/hero-bg.jpg')" }}>
+      <div
+        className="absolute top-0 bottom-auto scale-y-[-1] left-0 w-full h-[15px] bg-repeat bg-bottom"
+        style={{ backgroundImage: "url('/shape-grey.png')" }}
+      />
+
+      <div className="containerr md:grid md:grid-cols-2 md:items-center md:gap-[30px]">
+        <div>
+          <h2 className="h2 font-rubik text-[32px] text-white leading-[1.5] tracking-[-2px] max-w-[17ch] mx-auto md:mx-0 lg:text-[42px]">
+            {cta.title.text}{" "} <span className="text-n-yellow">{cta.title.highlight}</span>
+          </h2>
+          <p className="text-n-notgray text-[16px] mt-[25px] mb-[20px]">{cta.description}</p>
+          <Button>{cta.buttonText}</Button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default CTA;
