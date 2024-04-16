35df12c4c7f2fcef3d91755b8625fdaa262d8959
sorenblank <afrozaman12@gmail.com>
2024-04-16 10:39:17 +0600

    Footer Formatted to 2 spaces

--- app/pages/Footer.jsx
+++ app/pages/Footer.jsx
@@ -4,18 +4,18 @@ import { footer } from "../lib/constants"
 const Footer = () => {
   return (
     <section>
-        <div className='containerr relative py-6 flex flex-col gap-8 lg:flex-row lg:justify-between items-center align-middle'>
-            <p className='font-rubik'>{footer.copyrightText}</p>
-            <ul className='flex flex-wrap gap-5'>
-                {
-                    footer.socialLinks.map((link, index) => (
-                        <a key={index} className="flex items-center w-10 h-10 rounded-full bg-[#e7e7e7] hover:bg-[#cccccc]" href={link.url}>
-                            <Img src={link.icon} alt={link.name} width={16} height={16} className="mx-auto"/>
-                        </a>
-                    ))
-                }
-            </ul>
-        </div>
+      <div className='containerr relative py-6 flex flex-col gap-8 lg:flex-row lg:justify-between items-center align-middle'>
+        <p className='font-rubik'>{footer.copyrightText}</p>
+        <ul className='flex flex-wrap gap-5'>
+          {
+            footer.socialLinks.map((link, index) => (
+              <a key={index} className="flex items-center w-10 h-10 rounded-full bg-[#e7e7e7] hover:bg-[#cccccc]" href={link.url}>
+                <Img src={link.icon} alt={link.name} width={16} height={16} className="mx-auto" />
+              </a>
+            ))
+          }
+        </ul>
+      </div>
     </section>
   )
 }
